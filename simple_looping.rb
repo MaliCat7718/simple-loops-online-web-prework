f45a7577239f3f831f4417a9deacc223ff9d3930
Malary Robinson <robinmal16@gmail.com>
2019-04-20 19:44:55 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -8,7 +8,7 @@ end
 
 def times_iterator(number_of_times)
   7.times do
-  puts phrase = "Welcome to Flatiron School's Web Development Course!"
+  puts "Welcome to Flatiron School's Web Development Course!"
 end
 
 def while_iterator(number_of_times)
